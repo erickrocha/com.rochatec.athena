f657922db19e342d7580113ca94457a5ebacd569
Erick rocha <epr@ca115.synchro.com.br>
2014-09-23 10:29:44 -0300

    Signed-off-by: Erick rocha <epr@ca115.synchro.com.br>

--- customer/com.rochatec.athena.pdv.plugin/src/com/rochatec/athena/pdv/plugin/Activator.java
+++ customer/com.rochatec.athena.pdv.plugin/src/com/rochatec/athena/pdv/plugin/Activator.java
@@ -1,4 +1,4 @@
-package com.rochatec.athena.pdv;
+package com.rochatec.athena.pdv.plugin;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -10,7 +10,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "com.rochatec.athena.pdv"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "com.rochatec.athena.pdv.plugin"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
