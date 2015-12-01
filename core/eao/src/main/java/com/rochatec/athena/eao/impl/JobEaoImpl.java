3f867df56cfda84d1daa6252b02418141ac79fc3
Erick Rocha <erocha7@vmcommerce.intra>
2015-12-01 11:11:53 -0200

    Signed-off-by: Erick Rocha <erocha7@vmcommerce.intra>

--- core/eao/src/main/java/com/rochatec/athena/eao/impl/JobEaoImpl.java
+++ core/eao/src/main/java/com/rochatec/athena/eao/impl/JobEaoImpl.java
@@ -1,14 +1,13 @@
 package com.rochatec.athena.eao.impl;
 
-import java.io.Serializable;
-import java.util.List;
-
-import javax.ejb.Stateless;
-import javax.persistence.Query;
-
 import com.rochatec.athena.eao.local.JobEaoLocal;
 import com.rochatec.athena.eao.util.GenericEao;
 import com.rochatec.athena.model.Job;
+
+import javax.ejb.Stateless;
+import javax.persistence.Query;
+import java.io.Serializable;
+import java.util.List;
 
 @Stateless
 public class JobEaoImpl extends GenericEao<Job, Serializable> implements
@@ -21,7 +20,7 @@ public class JobEaoImpl extends GenericEao<Job, Serializable> implements
 
 	@SuppressWarnings("unchecked")
 	public List<Job> findByName(String name) {
-		Query query = getEntityManager().createNamedQuery("Job.findByName");
+		Query query = getEntityManager().createQuery("SELECT j FROM Job j WHERE j.name like :name");
 		query.setParameter("name", name + "%");
 		List<Job> jobs = (List<Job>) query.getResultList();
 		return jobs;
@@ -29,7 +28,7 @@ public class JobEaoImpl extends GenericEao<Job, Serializable> implements
 
 	@SuppressWarnings("unchecked")
 	public List<Job> findAll() {
-		Query query = getEntityManager().createNamedQuery("Job.findAll");
+		Query query = getEntityManager().createQuery("SELECT j FROM Job j");
 		List<Job> jobs = ((List<Job>) query.getResultList());
 		return jobs;
 	}
--- core/eao/src/test/java/com/rochatec/athena/eao/RoleEaoTest.java
+++ core/eao/src/test/java/com/rochatec/athena/eao/RoleEaoTest.java
@@ -1,17 +1,11 @@
 package com.rochatec.athena.eao;
 
-import com.rochatec.athena.eao.impl.RoleEaoImpl;
-import com.rochatec.athena.model.Role;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import java.util.Properties;
 
 /**
  * Created by erickpr on 24/11/15.
@@ -19,36 +13,12 @@ import java.util.Properties;
 @RunWith(MockitoJUnitRunner.class)
 public class RoleEaoTest {
 
-    private EntityManagerFactory emf;
+    @Mock
     private EntityManager entityManager;
-
-    @AfterClass
-    public void tearDownClass() throws Exception {
-        entityManager.close();
-        emf.close(); //close at application end
-    }
-
-    @BeforeClass
-    public void setUpClass() throws Exception {
-        Properties properties = new Properties();
-        properties.put("javax.persistence.jdbc.driver", "org.hsqldb.jdbcDriver");
-        properties.put("javax.persistence.jdbc.url", "jdbc:hsqldb:mem:.");
-        properties.put("javax.persistence.jdbc.user", "sa");
-        properties.put("javax.persistence.jdbc.password", "");
-        properties.put("hibernate.show_sql", "true");
-        properties.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
-        properties.put("hibernate.connection.shutdown", "true");
-        properties.put("hibernate.hbm2ddl.auto", "create-drop");
-        emf = Persistence.createEntityManagerFactory("AthenaPersistenceTest", properties);
-        entityManager = emf.createEntityManager();
-    }
 
     @Test
     public void shouldPersistRole() {
-        RoleEaoImpl roleEaoLocal = new RoleEaoImpl();
-        roleEaoLocal.setEntityManager(entityManager);
 
-        Role role = new Role();
 
     }
 }
